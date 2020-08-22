2a1b819453557e2943dc5df3f31e30f68af636f4
Slinkter <luis.j.cueva@gmail.com>
2020-08-22 10:49:17 -0500

    se agrego los drawable y colores

--- app/src/main/java/com/cudpast/arsiapp/MainActivity.java
+++ app/src/main/java/com/cudpast/arsiapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //todo:falta importa libreia de youtube q se registrar  y se descarga
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
