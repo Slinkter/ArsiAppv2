f89eab261735ad008a04859c5eae352af75f5901
devarsi <consultingarsiapp@gmail.com>
2020-12-28 10:45:44 -0500

    cambios de diseño

--- app/src/main/java/com/cudpast/arsiapp/Activities/BottomNavActivity.java
+++ app/src/main/java/com/cudpast/arsiapp/Activities/BottomNavActivity.java
@@ -24,6 +24,29 @@ public class BottomNavActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+
+
+
+
+        /*
+        *
+        *    <com.google.android.material.bottomnavigation.BottomNavigationView
+        android:id="@+id/nav_view"
+        android:layout_width="0dp"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="0dp"
+        android:layout_marginEnd="0dp"
+        android:background="?android:attr/windowBackground"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintLeft_toLeftOf="parent"
+        app:layout_constraintRight_toRightOf="parent"
+        app:menu="@menu/bottom_nav_menu" />
+        *
+        * */
+
+
     }
 
 }
